82c4a1bf58ed5c0e68ead71bb10db685bcfb21f1
Yurii Chudnovets <yurachudnovets@gmail.com>
2019-09-01 19:05:59 +0300

    -

---
+++ UserListTests/Modules/Saved/SavedUsersViewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  SavedUsersViewModelTests.swift
+//  UserListTests
+//
+//  Created by yura on 9/1/19.
+//  Copyright © 2019 yura. All rights reserved.
+//
+
+import XCTest
+import RxBlocking
+import RxSwift
+@testable import UserList
+
+class SavedUsersViewModelTests: BaseTestCase  {
+    
+    //var viewModel = SavedUsersViewModel(repository: <#T##UsersRepositoryProtocol#>)
+    
+}
